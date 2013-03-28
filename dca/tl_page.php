e9aeb3c445b84e3ef1b496658bffa821b7f3820d
Christian Barkowsky <mail@christianbarkowsky.de>
2013-03-28 17:08:15 +0100

    onload_callback added

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -11,11 +11,20 @@
 
 
 /**
+ * Config
+ */
+$GLOBALS['TL_DCA']['tl_page']['config']['onload_callback'][] = array('tl_page_canonical', 'changePalette');
+/**
  * Table tl_page
  */
+//$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'addCanonical';
+//$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace("{protected_legend:hide},", "{rel_canonical_legend},addCanonical;{protected_legend:hide},", $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
+//$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType,canonicalJumpTo,canonicalWebsite';
+
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'addCanonical';
 $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace("{protected_legend:hide},", "{rel_canonical_legend},addCanonical;{protected_legend:hide},", $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
-$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType,canonicalJumpTo,canonicalWebsite';
+//$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType,canonicalJumpTo,canonicalWebsite';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType';
 
 
 /**
@@ -37,7 +46,7 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['canonicalType'] = array
 	'inputType'               => 'select',
 	'options'       		 => array('internal', 'external'),
 	'reference'               => &$GLOBALS['TL_LANG']['PTY'],
-	'eval'                    => array('tl_class'=>'w50')
+	'eval'                    => array('mandatory'=>true, 'includeBlankOption'=>true, 'submitOnChange'=>true, 'tl_class'=>'w50')
 );
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['canonicalJumpTo'] = array
@@ -59,5 +68,30 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['canonicalWebsite'] = array
 	'inputType' => 'text',
 	'eval' => array('rgxp'=>'url', 'maxlength'=>255, 'tl_class'=>'long')
 );
+
+
+class tl_page_canonical extends Backend
+{
+	public function changePalette(DataContainer $dc)
+	{
+		if (!$dc->id)
+        {
+            return;
+        }
+	
+		$objPage = $this->Database->prepare("SELECT canonicalType FROM tl_page WHERE id=?")->limit(1)->execute($dc->id);
+        
+        if ($objPage->numRows > 0 AND $objPage->canonicalType == 'internal')
+		{
+			$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType,canonicalJumpTo';
+		}
+		
+		if ($objPage->numRows > 0 AND $objPage->canonicalType == 'external')
+		{
+			$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addCanonical'] = 'canonicalType,canonicalWebsite';
+		}
+	}
+}
+
 
 ?>
