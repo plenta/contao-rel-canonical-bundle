0cb2015280484b1408f476ee3d131b7388d472c7
Andreas Nölke <zero@brothers-project.de>
2015-09-16 14:42:00 +0200

    do not generate a 404 page cause the default module does this already

--- modules/ModuleEventReaderRelCannonical.php
+++ modules/ModuleEventReaderRelCannonical.php
@@ -28,32 +28,25 @@ class ModuleEventReader extends \Contao\ModuleEventReader
 	protected function compile()
 	{
 		global $objPage;
-	
+
 		// Get the current event
 		$objEvent = \CalendarEventsModel::findPublishedByParentAndIdOrAlias(\Input::get('events'), $this->cal_calendar);
 
 		if ($objEvent === null)
 		{
-			// Do not index or cache the page
-			$objPage->noSearch = 1;
-			$objPage->cache = 0;
-
-			// Send a 404 header
-			header('HTTP/1.1 404 Not Found');
-			$this->Template->event = '<p class="error">' . sprintf($GLOBALS['TL_LANG']['MSC']['invalidPage'], \Input::get('events')) . '</p>';
-			return;
+			parent::compile();
 		}
-		
+
 		$objPage->canonicalType = $objEvent->canonicalType;
 		$objPage->canonicalJumpTo = $objEvent->canonicalJumpTo;
 		$objPage->canonicalWebsite = $objEvent->canonicalWebsite;
-		
+
 		if($objEvent->canonicalType == 'self')
 		{
 			$objPage->canonicalType = 'external';
 			$objPage->canonicalWebsite = \Environment::get('url') . TL_PATH . '/' . \Environment::get('request');
 		}
 
-		parent::compile();	
+		parent::compile();
 	}
 }
